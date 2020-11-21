7a7de06f7888af2baa91ee82058f25b2645a9cd4
Ming Zhao <35346970+ming-zhao@users.noreply.github.com>
2020-11-21 18:35:36 -0500

    Create ml.py

---
+++ file/ml.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from ipywidgets import *
+from IPython.display import display, HTML
+import tensorflow as tf
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+print("hello")
