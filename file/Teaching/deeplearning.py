bec32dc026856b7b15cbb63e34f73f57e8ff2d78
Ming Zhao <35346970+ming-zhao@users.noreply.github.com>
2020-12-23 13:37:38 -0500

    Update deeplearning.py

--- file/Teaching/deeplearning.py
+++ file/Teaching/deeplearning.py
@@ -6,6 +6,10 @@ from ipywidgets import *
 from IPython.display import display, HTML
 import tensorflow as tf
 
+from keras.datasets import mnist
+(train_images, train_labels), (test_images, test_labels) = mnist.load_data()
+# print('training images:{}, test images:{}'.format(train_images.shape, test_images.shape))
+
 def showimg(data, idx):
     span = 5
     if data=='train':
@@ -27,7 +31,6 @@ def showimg(data, idx):
       plt.imshow(digit, cmap=plt.cm.binary)
       plt.title('Index:{}, Label:{}'.format(idx+i, labels[idx+i]), fontsize = 15)
     plt.show()
-
 
 from keras import models
 from keras import layers
