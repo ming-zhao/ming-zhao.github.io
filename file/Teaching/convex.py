831815ef49a473716c19e8a5f39c3319aaba7632
Ming Zhao <35346970+ming-zhao@users.noreply.github.com>
2020-12-23 15:35:34 -0500

    Update convex.py

--- file/Teaching/convex.py
+++ file/Teaching/convex.py
@@ -2,39 +2,47 @@ import numpy as np
 import matplotlib.pyplot as plt
 from ipywidgets import *
 
-def showconvex(values):
-    plt.subplots(2, 2, figsize=(17,10))
-    function = lambda x: (x-3)**2
-    x = np.linspace(0.8,4.2,500)
-    plt.subplot(2,2,1)
-    plt.plot(x, function(x), label='$f(x)$')
-    line = np.array(values)
-    plt.plot(line, function(line), 'o-')
-    plt.title('Convex: Line joining any two poits is above the curve')
-    
-    function = lambda x: np.log(x) - (x-2)**2
-    x = np.linspace(0.8,4.2,500)
-    plt.subplot(2,2,2)
-    plt.plot(x, function(x), label='$f(x)$')
-    line = np.array(values)
-    plt.plot(line, function(line), 'o-')
-    plt.title('Concave: Line joining any two poits is below the curve')
-    
-    function = lambda x: np.log(x) - 2*x*(x-4)**2
-    x = np.linspace(0.8,4.2,500)
-    plt.subplot(2,2,3)
-    plt.plot(x, function(x), label='$f(x)$')
-    line = np.array(values)
-    plt.plot(line, function(line), 'o-')
-    plt.title('Neither convex or concave')
-    
-    function = lambda x: np.cos(x*2)*x
-    x = np.linspace(0.8,4.2,500)
-    plt.subplot(2,2,4)
-    plt.plot(x, function(x), label='$f(x)$')
-    line = np.array(values)
-    plt.plot(line, function(line), 'o-')
-    plt.title('Neither convex or concave')    
-    
-    plt.legend()
-    plt.show()    
+def convex():
+    def showconvex(values):
+        plt.subplots(2, 2, figsize=(17,10))
+        function = lambda x: (x-3)**2
+        x = np.linspace(0.8,4.2,500)
+        plt.subplot(2,2,1)
+        plt.plot(x, function(x), label='$f(x)$')
+        line = np.array(values)
+        plt.plot(line, function(line), 'o-')
+        plt.title('Convex: Line joining any two poits is above the curve')
+        
+        function = lambda x: np.log(x) - (x-2)**2
+        x = np.linspace(0.8,4.2,500)
+        plt.subplot(2,2,2)
+        plt.plot(x, function(x), label='$f(x)$')
+        line = np.array(values)
+        plt.plot(line, function(line), 'o-')
+        plt.title('Concave: Line joining any two poits is below the curve')
+        
+        function = lambda x: np.log(x) - 2*x*(x-4)**2
+        x = np.linspace(0.8,4.2,500)
+        plt.subplot(2,2,3)
+        plt.plot(x, function(x), label='$f(x)$')
+        line = np.array(values)
+        plt.plot(line, function(line), 'o-')
+        plt.title('Neither convex or concave')
+        
+        function = lambda x: np.cos(x*2)*x
+        x = np.linspace(0.8,4.2,500)
+        plt.subplot(2,2,4)
+        plt.plot(x, function(x), label='$f(x)$')
+        line = np.array(values)
+        plt.plot(line, function(line), 'o-')
+        plt.title('Neither convex or concave')    
+        
+        plt.legend()
+        plt.show()
+    interact(showconvex,
+             values = widgets.FloatRangeSlider(value=[2, 3.5],
+                                               min=1.0, max=4.0, step=0.1,
+                                               description='x values:', disabled=False,
+                                               continuous_update=False,
+                                               orientation='horizontal',readout=True,
+                                               readout_format='.1f'));        
